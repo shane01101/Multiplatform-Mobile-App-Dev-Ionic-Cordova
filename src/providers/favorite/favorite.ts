ce824a45540facf8cf3828be2329a60c3005d1ed
Shane Honanie <shaneh425@gmail.com>
2017-11-26 15:34:53 -0800

    Assignment03

--- src/providers/favorite/favorite.ts
+++ src/providers/favorite/favorite.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Dish } from '../../shared/dish';
 import { Observable } from 'rxjs/Observable';
 import { DishProvider } from '../dish/dish';
+import { Storage } from '@ionic/storage';
 
 @Injectable()
 export class FavoriteProvider {
@@ -10,14 +11,27 @@ export class FavoriteProvider {
 	favorites: Array<any>;
 
 	constructor(public http: Http,
-		private dishservice: DishProvider) {
+		private dishservice: DishProvider,
+		private storage: Storage) {
 			console.log('Hello FavoriteProvider Provider');
 			this.favorites = [];
+
+			storage.get('favorites').then(favorites => {
+				if (favorites) {
+				  console.log(favorites);
+				  this.favorites = favorites;
+				}
+				else
+				  console.log('favorites not defined');
+			  });
 	}
 
 	addFavorite(id: number): boolean {
 		if (!this.isFavorite(id))
+		{
 			this.favorites.push(id);
+			this.storage.set('favorites', this.favorites);
+		}	
 		console.log('favorites', this.favorites);
 		return true;
 	}
@@ -35,6 +49,7 @@ export class FavoriteProvider {
 		let index = this.favorites.indexOf(id);
 		if (index >= 0) {
 			this.favorites.splice(index,1);
+			this.storage.set('favorites', this.favorites);
 			return this.getFavorites();
 		}
 		else {
